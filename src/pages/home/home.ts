cafb4f500c226484b4858feaea8107fc61dab256
Andrew Burns <anditburns@gmail.com>
2019-08-31 00:50:52 +0800

    initial committ

---
+++ src/pages/home/home.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { NavController, AlertController  } from 'ionic-angular';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  constructor(public navCtrl: NavController, 
+              public alertController: AlertController ) {
+
+  }
+  
+  async calc(){
+	  const alert = await this.alertController.create({
+      header: 'Alert',
+      subHeader: 'Subtitle',
+      message: 'This is an alert message.',
+      buttons: ['Cancel', 'Open Modal', 'Delete']
+    });
+
+    await alert.present();
+  }
+
+}
