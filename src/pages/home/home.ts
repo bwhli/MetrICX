313bdb3e6ed18664f72c4066d3f178fada484614
Andrew Burns <anditburns@gmail.com>
2019-09-01 21:14:11 +0800

    adding icon-sdk

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,25 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, Injectable  } from '@angular/core';
 import { NavController, AlertController  } from 'ionic-angular';
+import IconService, { HttpProvider } from 'icon-sdk-js';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
 export class HomePage {
 
-  constructor(public navCtrl: NavController, 
-              public alertController: AlertController ) {
-
-  }
+  constructor(public navCtrl: NavController,   
+			  public alertController: AlertController) 
+			  { }
   
   async calc(){
+	 
+	  const httpProvider = new HttpProvider('https://ctz.solidwallet.io/api/v3');
+	  const service = new IconService(httpProvider);
+
+	  const balance = await service.getBalance('hx902ecb51c109183ace539f247b4ea1347fbf23b5').execute();
+	  	  
+	  console.log(httpProvider);
 	  const alert = await this.alertController.create({
-      title: 'Alert',
-      message: 'This is an alert message.',
-      buttons: ['Cancel', 'Open Modal', 'Delete']
+      title: 'ICX Balance',
+      message: balance,
+      buttons: ['Cancel']
     });
 
     await alert.present();
+	
   }
 
 }
