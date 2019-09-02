9990de3435dd4ac3824ed106358746e02afdfe92
Andrew Burns <anditburns@gmail.com>
2019-09-02 22:04:44 +0800

    no message

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,9 @@
 import { Component, Injectable  } from '@angular/core';
 import { NavController, AlertController  } from 'ionic-angular';
-import IconService, { HttpProvider } from 'icon-sdk-js';
+import IconService, { HttpProvider, IconBuilder, IconConverter, IconAmount  } from 'icon-sdk-js';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+const { CallBuilder } = IconBuilder;
 
 @Component({
   selector: 'page-home',
@@ -8,27 +11,52 @@ import IconService, { HttpProvider } from 'icon-sdk-js';
 })
 
 export class HomePage {
-
+   
+  public stakingForm: FormGroup;
+  
   constructor(public navCtrl: NavController,   
-			  public alertController: AlertController) 
-			  { }
+			  public alertController: AlertController,
+			  public formBuilder: FormBuilder) 
+			  { 
+				this.stakingForm = formBuilder.group({
+					amount: [null],
+					network_perc: [null],
+					address: [null]
+				});
+			  }
   
   async calc(){
 	 
 	  const httpProvider = new HttpProvider('https://ctz.solidwallet.io/api/v3');
-	  const service = new IconService(httpProvider);
+	  const iconService = new IconService(httpProvider);
+     
+	  const balance = await iconService.getBalance('hx5af8bd1b5cdc321a53ead7a836601ce8e2c938c9').execute();
+	  
+	  const call = new CallBuilder()
+				.to('cx0000000000000000000000000000000000000000')
+				.method('getStake')
+				.params({
+							address: 'hx1141b769011ee8399ef70f393b568ca15a6e22d7'
+						})				
+                .build();
 
-	  const balance = await service.getBalance('hx902ecb51c109183ace539f247b4ea1347fbf23b5').execute();
-	  	  
-	  console.log(httpProvider);
+     const getStake = await iconService.call(call).execute();
+	 const stake = getStake['stake'];
+	
+	 console.log(IconConverter.toBigNumber(IconAmount.of(stake, IconAmount.Unit.ICX).convertUnit(IconAmount.Unit.LOOP)));
+
 	  const alert = await this.alertController.create({
       title: 'ICX Balance',
-      message: balance,
+      message: IconConverter.toBigNumber(stake).toString(),
       buttons: ['Cancel']
     });
 
     await alert.present();
 	
   }
+  
+  
+  
+
 
 }
