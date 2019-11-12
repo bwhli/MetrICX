cc1c6cb042f71270d0c08fab0d413ca57632074d
salvation-al <alton.turner@gmail.com>
2019-11-12 21:57:06 +0800

    Sample files of private secrets. You should have locally a copy of these without the .Sample bit and never checked in

---
+++ firebase-config.sample.ts
@@ -0,0 +1,15 @@
+// Your web app's Firebase configuration
+// Download the config from Firebase-Web project
+
+export class FirebaseConfig {
+  public static config = {
+    apiKey: "xxx",
+    authDomain: "xxxxx.firebaseapp.com",
+    databaseURL: "https://xxxxx.firebaseio.com",
+    projectId: "xxxxx",
+    storageBucket: "xxxxx.appspot.com",
+    messagingSenderId: "xxxxx",
+    appId: "1:xxxxx:web:xxxxx",
+    measurementId: "G-xxxxx"
+  };
+}
