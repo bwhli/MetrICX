e1cbad46bebde7b3f8a71dfaa2ab2790e9a90249
salvation-al <34540631+salvation-al@users.noreply.github.com>
2019-11-12 15:42:12 +0800

    Feature/enable basic push (#11)
    * Initial Checkin of Push
    
    * Fix merge issues
    
    * Remove .gradle
    
    * 2 new settings to enable Push per client

---
+++ src/app/services/fcm/fcm.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { Firebase } from '@ionic-native/firebase/ngx';
+import { Platform } from '@ionic/angular';
+import { AngularFirestore } from 'angularfire2/firestore';
+
+@Injectable()
+export class FcmService {
+
+  constructor(private firebase: Firebase,
+              private afs: AngularFirestore,
+              private platform: Platform) {}
+
+  async getToken():Promise<string> {
+    let token: string;
+
+    if (this.platform.is('android')) {
+      token = await this.firebase.getToken();
+    }
+
+    if (this.platform.is('ios')) {
+      token = await this.firebase.getToken();
+      await this.firebase.grantPermission();
+    }
+
+    return token;
+  }
+
+  onNotifications() {
+    return this.firebase.onNotificationOpen();
+  }
+}
