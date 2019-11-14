88efde932c14c91f71d16eec87a5092735614a8f
salvation-al <34540631+salvation-al@users.noreply.github.com>
2019-11-15 06:32:12 +0800

    ..

--- src/app/settings/settings.module.ts
+++ src/app/settings/settings.module.ts
@@ -4,13 +4,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { SettingsPage } from './settings.page';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { NgxQRCodeModule } from 'ngx-qrcode2';
 
+library.add(fas);
 @NgModule({
   imports: [
     IonicModule,
     CommonModule,
     FormsModule,
+    FontAwesomeModule,
     ReactiveFormsModule,
+    NgxQRCodeModule,
     RouterModule.forChild([{ path: '', component: SettingsPage }])
   ],
   declarations: [SettingsPage]
