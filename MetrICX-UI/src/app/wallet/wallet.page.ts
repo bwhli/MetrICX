f0650380ea9b3600846b2bfe549a23b4b1d03ea6
Firefight-Rhizome <andrew@rhizomeicx.com>
2019-12-04 22:02:29 +1100

    only show the unstake timer card if the user has an unstake claim waiting.

--- MetrICX-UI/src/app/wallet/wallet.page.ts
+++ MetrICX-UI/src/app/wallet/wallet.page.ts
@@ -28,6 +28,8 @@ export class WalletPage implements OnInit {
   public rewardRate = 0;
   public monthlyICX = 0;
   public yearlyICX = 0;
+  public hideUnstakeTimer: boolean = true;
+  public rowSize: number = 6;
 
   constructor(
     private storage: Storage,
@@ -84,9 +86,12 @@ export class WalletPage implements OnInit {
    const hours = await this.iconContract.getUnstakedPeriod(this.address);
    if (hours > 0) {
      const splitTime = this.splitTime(hours);
-     this.unstakePeriod = splitTime[0]['d'] + 'd: ' + splitTime[0]['h'] + 'h: ' + splitTime[0]['m'] + 'm';;
+     this.unstakePeriod = splitTime[0]['d'] + 'd: ' + splitTime[0]['h'] + 'h: ' + splitTime[0]['m'] + 'm';
+     this.rowSize = 6;
+     this.hideUnstakeTimer = false;
    } else {
-     this.unstakePeriod = 'N/A';
+     this.hideUnstakeTimer = true;
+     this.rowSize = 12;
    }
   }
 
