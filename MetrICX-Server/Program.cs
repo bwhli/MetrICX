43b4a29ecdee0eff68c0996563294ca07fab5909
salvation-al <alton.turner@gmail.com>
2020-01-24 21:47:08 +0800

    a

--- MetrICX-Server/Program.cs
+++ MetrICX-Server/Program.cs
@@ -19,15 +19,15 @@ namespace MetrICXServerPush
 
         static void Main(string[] args)
         {
-            var device = FirebaseGateway.GetDevice("c7H-TUPSWYM:APA91bFV_WsMgSaz3XhWNj5y6ixppxkEAWDtkCDuRK5CnS2N-heopE3xyHD0DE9G6qZFhjb_6NWHZxJIzipRN40gWCRyl1biYEdhkVLNDAwgLKI9FHjikHZbstbT8v4DWvT1rxMmZph1");
-            device.addresses[0].tokens = new System.Collections.Generic.List<Token>() { new Token() {token = "TAP", contractAddress = "cxc0b5b52c9f8b4251a47e91dda3bd61e5512cd782" } }; 
+            //var device = FirebaseGateway.GetDevice("c7H-TUPSWYM:APA91bFV_WsMgSaz3XhWNj5y6ixppxkEAWDtkCDuRK5CnS2N-heopE3xyHD0DE9G6qZFhjb_6NWHZxJIzipRN40gWCRyl1biYEdhkVLNDAwgLKI9FHjikHZbstbT8v4DWvT1rxMmZph1");
+            //device.addresses[0].tokens = new System.Collections.Generic.List<Token>() { new Token() {token = "TAP", contractAddress = "cxc0b5b52c9f8b4251a47e91dda3bd61e5512cd782" } }; 
             
-            ProcessDeviceAddress(device, device.addresses[0]);
+            //ProcessDeviceAddress(device, device.addresses[0]);
 
-            foreach (var token in device.addresses[0].tokens)
-            {
-                ProcessDeviceToken(device, token);
-            }
+            //foreach (var token in device.addresses[0].tokens)
+            //{
+            //    ProcessDeviceToken(device, token);
+            //}
 
             Console.WriteLine("[MAIN] STARTING APPLICATION TIMER  v2.1");
             timer.Elapsed += Timer_Elapsed;
