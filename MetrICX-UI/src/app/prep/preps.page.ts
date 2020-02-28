bf8648e510ff9de4eab919a13c69c09a4930a801
Andrew Burns <53776307+Firefight-Rhizome@users.noreply.github.com>
2020-02-28 18:25:35 +0800

    Multi Address Swipe for Tokens Page (#48)
    * token swipe
    
    * circle progress chart a little too small
    
    * fixing bug in the presentLoading controller when the dismiss fails.
    
    * fixing individual voting %
    
    * removing unused function

--- MetrICX-UI/src/app/prep/preps.page.ts
+++ MetrICX-UI/src/app/prep/preps.page.ts
@@ -84,13 +84,26 @@ export class PrepsPage  {
   }
 
   async presentLoading() {
-    const loading = await this.loadingController.create({
+    this.isLoaded = true;
+    await this.loadingController.create({
       spinner: null,
       message: '<ion-img src="/assets/loading-spinner-trans.gif" alt="loading..."></ion-img>',
       cssClass: 'loading-css',
-      showBackdrop: false
+      showBackdrop: false,
+      duration: 5000
+    }).then(a => {
+      a.present().then(() => {
+        console.log('presented');
+        if (!this.isLoaded) {
+          a.dismiss().then(() => console.log('abort presenting'));
+        }
+      });
     });
-    await loading.present();
+  }
+
+  async dismiss() {
+    this.isLoaded = false;
+    return await this.loadingController.dismiss().then(() => console.log('dismissed'));
   }
 
   async filterPrepsList(delegatedPrepList: Delegations[], allPreps: PReps) : Promise<PrepDetails[]> {
@@ -108,22 +121,22 @@ export class PrepsPage  {
     var totalSupply = await this.iconContract.getTotalSupply();
     var votedPreps: number = delegatedPReps.delegations.length;
    
-    var prepData = new PrepPie();
-    prepData.name = [];
-    prepData.value = [];
+    var prepData: PrepPie[] = [];
     let numPreps = 0;
     for(var i = 0; i < votedPreps; i++) {
+      var pData = new PrepPie();
       numPreps++;
-      prepData.value[i] = delegatedPReps.delegations[i].value;
+      pData.value = delegatedPReps.delegations[i].value;
       var prep = await this.iconContract.getPRep(delegatedPReps.delegations[i].address);
       var prepName = prep['name'];
-      prepData.name[i] = prepName;
+      pData.name = prepName;
+      prepData.push(pData);
     }
 
     this.numberOfVotedReps = numPreps;
     var delegatedPrepDetail = await this.filterPrepsList(delegatedPReps.delegations, preps);   
 
-    await this.createTableData(delegatedPrepDetail, preps.totalDelegated, prepData.value);
+    await this.createTableData(delegatedPrepDetail, preps.totalDelegated, prepData);
 
     this.totalSupply = Math.round(totalSupply).toLocaleString();
     this.totalICXDelegated = Math.round(preps.totalDelegated).toLocaleString();
@@ -134,16 +147,15 @@ export class PrepsPage  {
     this.lastBlockCreatedBy = await this.iconContract.getLastBlockCreatedBy();
 
     this.totalNumPreps = preps.preps.length;
-    this.loadingController.dismiss();
+    await this.dismiss();
+
    }
 
-  async createTableData(prepDetail: PrepDetails[], totalDelegated: number, votingPerc: number[]) {
+  async createTableData(prepDetail: PrepDetails[], totalDelegated: number, prepDataVoted: PrepPie[]) {
     var prepArray: PrepTable[] = [];
     let totalVoted: number = 0;
 
-    for(var i = 0; i < votingPerc.length; i++) {
-      totalVoted = totalVoted + votingPerc[i];
-    }
+   
 
     for(var i = 0; i < prepDetail.length; i++) {
       var prepTable = new PrepTable();
@@ -158,12 +170,19 @@ export class PrepsPage  {
       if(!productivityPerc) {
         productivityPerc = 0; 
       }
+
+      for(var j = 0; j < prepDataVoted.length; j++) {
+        totalVoted = totalVoted + prepDataVoted[i].value;
+        if(prepDetail[i].name == prepDataVoted[j].name) {
+          prepTable.myVotes = prepDataVoted[j].value;
+          prepTable.width = prepDataVoted[j].value / totalVoted * 100;
+        }
+      }
       
-       prepTable.myVotes = votingPerc[i];
+   
        prepTable.city = prepDetail[i].city;
        prepTable.totalVotes = (Math.round(prepDetail[i].delegated *100)/100).toLocaleString();
        prepTable.production = Math.round(productivityPerc * 100)/100;
-       prepTable.width = votingPerc[i] / totalVoted * 100;
        prepArray.push(prepTable);
     }
 
