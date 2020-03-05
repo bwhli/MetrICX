d2a65dceb8688b30834be6886434715d1b99c5fe
Firefight-Rhizome <andrew@rhizomeicx.com>
2020-03-05 23:47:27 +0800

    fixing up some icons and fonts

--- MetrICX-UI/src/app/tabs/tabs.module.ts
+++ MetrICX-UI/src/app/tabs/tabs.module.ts
@@ -4,7 +4,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faWallet, faCogs, faBoxBallot, faCoins }  from '@fortawesome/pro-light-svg-icons';
+import { faWallet, faCogs, faPollPeople, faCoins }  from '@fortawesome/pro-light-svg-icons';
 import { TabsPageRoutingModule } from './tabs.router.module';
 import { TabsPage } from './tabs.page';
 
@@ -20,6 +20,6 @@ import { TabsPage } from './tabs.page';
 })
 export class TabsPageModule {
 constructor() {
-     library.add(faWallet, faCogs, faBoxBallot, faCoins );
+     library.add(faWallet, faCogs, faPollPeople, faCoins);
   }  
 }
