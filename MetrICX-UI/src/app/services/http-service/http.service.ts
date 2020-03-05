a28bd8d85e925402e3d7ff310e6ba5cae0e45f9b
Firefight-Rhizome <andrew@rhizomeicx.com>
2020-03-06 07:23:07 +0800

    no message

--- MetrICX-UI/src/app/services/http-service/http.service.ts
+++ MetrICX-UI/src/app/services/http-service/http.service.ts
@@ -10,26 +10,9 @@ export class HttpService  {
     imageUrl: string;
     logo: string;
 
-    constructor(private http: HTTP) { }
-
-     async get(url: string) : Promise<string> {
-        const params = {};
-        const headers = {};
-        return await this.http.get(url,params,headers).then(function (response) { 
-        try {
-            response.data = JSON.parse(response.data);
-            return response.data.representative.logo.logo_256;
-        }
-        catch {
-            //unable to parse the json for some reason.
-            return '';
-        }
-        //   return response.data.representative.logo.logo_256; //['representative']['logo']['logo_256'];
-            // this.logo = data['representative']['logo']['logo_256'];
-        }, function(response) {
-            //something went wrong with the htpp request
-            console.log(response.status);
-        });
+    constructor(private http: HttpClient) { }
+     async get() {
+        return await this.http.get('https://tracker.icon.foundation/v3/main/mainInfo').toPromise();
 
     }
 }
