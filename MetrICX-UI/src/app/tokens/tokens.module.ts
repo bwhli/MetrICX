cadf01ac3272231f8b703dbe395b405f6d4a449e
Firefight-Rhizome <andrew@rhizomeicx.com>
2020-03-13 15:35:57 +0800

    commenting out

--- MetrICX-UI/src/app/tokens/tokens.module.ts
+++ MetrICX-UI/src/app/tokens/tokens.module.ts
@@ -1,4 +1,4 @@
-import { IonicModule } from '@ionic/angular';
+/*import { IonicModule } from '@ionic/angular';
 import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
@@ -22,3 +22,4 @@ export class TokensPageModule {
     library.addIconPacks(fas);
  } 
 }
+*/
