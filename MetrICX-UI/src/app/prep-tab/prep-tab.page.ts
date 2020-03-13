83d2d4a8c1f6d9f440ca4ccec16528a005f21d10
Firefight-Rhizome <andrew@rhizomeicx.com>
2020-03-13 15:51:36 +0800

    prep pages

--- MetrICX-UI/src/app/prep-tab/prep-tab.page.ts
+++ MetrICX-UI/src/app/prep-tab/prep-tab.page.ts
@@ -1,7 +1,9 @@
-import { Component} from '@angular/core';
+import { Component, ViewChild} from '@angular/core';
 import { NavController } from '@ionic/angular';
 import { LoadingController } from '@ionic/angular';
 import { SettingsService } from '../services/settings/settings.service';
+import { SuperTabs } from '@ionic-super-tabs/angular';
+import { Address } from '../services/settings/settings';
 
 @Component({
   selector: 'app-prep-tab',
@@ -10,15 +12,40 @@ import { SettingsService } from '../services/settings/settings.service';
 })
 export class PrepTabPage {
 
-  public Addresses: any;
+  @ViewChild('superTabs', { static: false, read: SuperTabs }) st: SuperTabs;
+
+  public Addresses: any //MapArray<Address>;
+  public activeTabIndex: number;
+
+  public address_0: Address;
+  public address_1: Address;
+  public address_2: Address;
+  public address_3: Address;
+  public address_4: Address;
 
   constructor(
     public loadingController: LoadingController,
     public navCtrl: NavController,
-    private settingsService: SettingsService
+    public settingsService: SettingsService    
   ) { }
 
   async ionViewWillEnter() {
-    this.Addresses = (await this.settingsService.get()).addresses_v2;
+    var length = await this.settingsService.getLength();
+    if (length == 0) {
+      this.navCtrl.navigateForward('/tabs/settings');
+      return;
+    }
+    this.address_0 = await this.settingsService.getByIndex(0);
+    if (length > 1) this.address_1 = await this.settingsService.getByIndex(1);
+    if (length > 2) this.address_2 = await this.settingsService.getByIndex(2);
+    if (length > 3) this.address_3 = await this.settingsService.getByIndex(3);
+    if (length > 4) this.address_4 = await this.settingsService.getByIndex(4);
+  }
+
+  async onTabChange(newTabIndexEvent: CustomEvent<number>) {
+    var length = await this.settingsService.getLength();
+    if (newTabIndexEvent.detail > length - 1) {
+      setTimeout(()=> this.st.selectTab(0, true), 100);
+    }
   }
 }
